626075f82149a9b9160e3b222863494e2edbe11d
Brubru <jordan.sankare@gmail.com>
2024-09-26 17:12:14 +0200

    first commit

---
+++ src/modal.ts
@@ -0,0 +1,19 @@
+const modal = document.getElementById('modal');
+const btn = document.getElementById('openModal');
+const span = document.getElementById('close');
+
+if(btn){
+    btn.onclick = function() {
+        if (modal){
+            modal.style.display = 'block';
+        }
+    }
+}
+
+if(span){
+    span.onclick = function() {
+        if (modal){
+            modal.style.display = 'none';
+        }
+    }
+}
