6380f5ecb74519f8c4161b56d7243c7bdf2b0e9c
jordan sankare <jordan.sankare@gmail.com>
2025-01-04 18:55:49 +0100

    upgrade dashboard

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { getCurrentUser } from "./core/auth/getCurrentUser.js";
 import { Transaction, isTransaction } from "./pages/transactions.js";
 import { Category, isCategory } from "./pages/categories.js";
 import { Budget, isBudget } from "./pages/budgets.js";
+import { log } from "console";
 
 function onUserInteraction(eventType: string) {
   console.log(`User interaction detected: ${eventType}`);
@@ -377,8 +378,40 @@ function renderCategoryExpensesChart(labels: string[], data: number[]): void {
 // Charger les statistiques après le chargement de la page
 document.addEventListener("DOMContentLoaded", async () => {
   const userId = await getUser();
-  if (userId) generateStatistics(userId);
+  if (userId) {
+    generateStatistics(userId);
+    const userImgMarkup = document.getElementById("dashboard-pic") as HTMLImageElement;
+    const userNameMarkup = document.getElementById("dashboard-name") as HTMLImageElement;
+    if (userImgMarkup) {
+      const db = await openDatabase("UserDatabase", "users");
+      const userEmail = localStorage.getItem("userMail");
+
+      if (userEmail) {
+        try {
+          const user = await getCurrentUser(db, userEmail);
+          if (user) {
+            if (user.picture) {
+              userImgMarkup.src = user.picture; // Set the image path to user's picture
+              } else {
+                userImgMarkup.src = "https://picsum.photos/200?grayscale";
+              }
+              if (user.firstname) {
+                userNameMarkup.innerHTML = user.firstname.charAt(0).toUpperCase() + user.firstname.slice(1);;
+              } else {
+                userNameMarkup.innerHTML = "Inconnu"
+              }
+            }
+        } catch (error) {
+          console.error("Error retrieving user information:", error);
+        }
+      } else {
+        console.error("No user email found in localStorage.");
+      }
+    }
+  }
 });
+
+
 
 async function getUser(): Promise<number | null> {
   const db = await openDatabase("UserDatabase", "users");
